cd8fa5058679220e93ba8ba27c369eb2654838d2
Paul Curtis <paul_curtis@cooperators.ca>
2016-07-30 17:20:58 -0400

    GC
    added in some garbage collection code

--- TalkingData.R
+++ TalkingData.R
@@ -24,6 +24,7 @@ mergedevents <- merge(mergedevents, phone_brand_device_model, all.x=TRUE, by="de
 setkey(app_event, event_id)
 allevents <- merge(app_event, mergedevents, all=TRUE, by="event_id")
 rm(mergedevents)
+gc()
 
 setkey(app_labels, app_id)
 # there are duplicate app_ids in app_labels this statement removes duplciates.
@@ -31,12 +32,26 @@ setkey(app_labels, app_id)
 app_labels <- unique(app_labels)
 alleventsapps <- merge(allevents, app_labels, all.x=TRUE, by="app_id")
 rm(allevents)
+gc()
 
 #setkey(alleventsapps, label_id)
 setkey(label_categories, label_id)
 alleventsappslabels <- merge(alleventsapps, label_categories, all.x=TRUE, by="label_id")
 rm(alleventsapps)
+gc()
 
 #hist(alleventsappslabels$age)
+
+rm(app_event)
+rm(app_labels)
+rm(events)
+rm(gender_age_test)
+rm(gender_age_train)
+rm(label_categories)
+rm(phone_brand_device_model)
+rm(sample_submission)
+
+gc()
+
 
 
